46d8bf8e0d13e8e29806e5635e7961badfb66fed
Artem.Baranovskyi <you@example.com>
2023-05-20 15:49:06 +0300

    Second exercise is done (verifiable presentation).

--- src/create-credential.ts
+++ src/create-credential.ts
@@ -1,7 +1,8 @@
-import { agent } from './veramo/setup.js'
+import { agent } from './veramo/setup.js';
 
 async function main() {
-  const identifier = await agent.didManagerGetByAlias({ alias: 'exercise-1' })
+  const identifier = await agent.didManagerGetByAlias({ alias: 'exercise-1' });
+  const currentDate = new Date().toISOString();
 
   const verifiableCredential = await agent.createVerifiableCredential({
     credential: {
@@ -14,9 +15,73 @@ async function main() {
       },
     },
     proofFormat: 'jwt',
-  })
-  console.log(`New credential created`)
-  console.log(JSON.stringify(verifiableCredential, null, 2))
+  });
+
+  const createCredential = async (type: string, credentialSubject: any) => {
+    return agent.createVerifiableCredential({
+      fetchRemoteContexts: false,
+      keyRef: "",
+      proofFormat: 'jwt',
+      removeOriginalFields: false,
+      save: false,
+      credential: {
+        id: `http://example.com/credentials/${type}`,
+        type: ['VerifiableCredential', type],
+        issuer: { id: identifier.did },
+        issuanceDate: currentDate,
+        credentialSubject,
+      },
+    });
+  };
+
+  const masterDegreeCredential = await createCredential('MasterDegreeCredential', {
+    degree: 'Master of Science',
+    university: 'HFT Stuttgart',
+    graduationYear: 2024,
+  });
+
+  const residencePermitCredential = await createCredential('ResidencePermitCredential', {
+    permitNumber: '123456789',
+    country: 'Federal Republic of Germany',
+    validUntil: '2025-12-31',
+  });
+
+  const jobPermitCredential = await createCredential('JobPermitCredential', {
+    permitNumber: '987654321',
+    company: 'HFT Stuttgart',
+    position: 'Software Engineer',
+    validUntil: '2029-06-30',
+  });
+
+  const verifiablePresentation = await agent.createVerifiablePresentation({
+    challenge: "",
+    domain: "",
+    fetchRemoteContexts: false,
+    keyRef: "",
+    proofFormat: 'jwt',
+    removeOriginalFields: false,
+    save: false,
+    presentation: {
+      id: 'http://example.com/presentations/123',
+      type: ['VerifiablePresentation', 'JobApplicationPresentation'],
+      issuer: {
+        id: identifier.did,
+      },
+      holder: identifier.did,
+      issuanceDate: currentDate,
+      verifiableCredential: [
+        masterDegreeCredential.proof.jwt,
+        residencePermitCredential.proof.jwt,
+        jobPermitCredential.proof.jwt,
+      ],
+    },
+  });
+
+  console.log(`New credential created`);
+  console.log(JSON.stringify(verifiableCredential, null, 2));
+
+  console.log(`New Verifiable Presentation created:`);
+  console.log(JSON.stringify(verifiablePresentation, null, 2));
 }
 
-main().catch(console.log)
+main().catch(console.log);
